211cea7ee7aaf28b1e297424a984e5c092ddd292
Mohamed Nagy <postta@aol.com>
2023-01-02 18:55:15 +0200

    change settings try

--- project/settings.py
+++ project/settings.py
@@ -379,7 +379,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # If you don't have this directory and have this uncommented your build will fail
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 #STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles/css"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "_static/css"),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT = (os.path.join(SITE_ROOT, 'static_files/'))
 TEMPLATE_DIRS = (
