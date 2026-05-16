2831bee40f62b66c9dd7289fb622ba4b751040e2
Mohamed Nagy <postta@aol.com>
2026-05-17 02:48:54 +0300

    fixing bugs

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 # build_files.sh
 #!/bin/bash
-
 echo "Installing Python dependencies..."
 python3 -m pip install -r requirements.txt
-
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
