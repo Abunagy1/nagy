64546f847eeab05dfba5ee55660400c20815b853
Mohamed Nagy <postta@aol.com>
2026-05-17 10:41:02 +0300

    fix builds

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 echo "Starting build.sh..."
 
-echo "Installing Python dependencies with upgrade strategy..."
-python3 -m pip install --break-system-packages --upgrade --upgrade-strategy eager -r requirements.txt
+echo "Upgrading pip and setuptools..."
+python3 -m pip install --break-system-packages --upgrade pip setuptools
+
+echo "Installing Python dependencies..."
+python3 -m pip install --break-system-packages --upgrade -r requirements.txt
+
+# echo "Installing Python dependencies with upgrade strategy..."
+# python3 -m pip install --break-system-packages --upgrade --upgrade-strategy eager -r requirements.txt
 
 echo "Running database migrations..."
 python3 manage.py migrate --noinput
